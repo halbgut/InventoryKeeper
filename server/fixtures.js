b15f7160cc52e0de0281a93027f6638eaf9f6a1f
Kriegslustig <github@ls7.ch>
2015-03-05 21:55:02 +0100

    Add fixtures for Bookings

--- server/fixtures.js
+++ server/fixtures.js
@@ -7,15 +7,52 @@ if(Objects.find({}).count() < 1) {
   Objects.insert({
     name: 'Panasonic Camera'
   , objectType: 'Camera'
-  , serialNo: 'XYS9448'
+  , serialNo: 'XZS9480'
   })
   Objects.insert({
     name: 'Rode Mic'
   , objectType: 'Mic'
   , serialNo: '2391DSG'
   })
+
   Accounts.createUser({
     username: 'superman'
   , password: 'megasicher'
+  })
+
+  Bookings.insert({
+    user: Meteor.users.findOne({})._id
+  , description: 'Engadiner Ski Marathon'
+  , timeRange: {
+      from: currentTimeStamp + 10000
+    , to: currentTimeStamp + 30000
+    }
+  , subjects: [
+      Objects.findOne({serialNo: 'XYS9448'})._id
+    ]
+  })
+  Bookings.insert({
+    user: Meteor.users.findOne({})._id
+  , description: 'Some Event'
+  , timeRange: {
+      from: currentTimeStamp + 40000
+    , to: currentTimeStamp + 60000
+    }
+  , subjects: [
+      Objects.findOne({serialNo: 'XZS9480'})._id
+    , Objects.findOne({serialNo: 'XYS9448'})._id
+    ]
+  })
+  Bookings.insert({
+    user: Meteor.users.findOne({})._id
+  , description: 'Another Event'
+  , timeRange: {
+      from: currentTimeStamp + 15000
+    , to: currentTimeStamp + 30000
+    }
+  , subjects: [
+      Objects.findOne({serialNo: '2391DSG'})._id
+    , Objects.findOne({serialNo: 'XYS9448'})._id
+    ]
   })
 }
