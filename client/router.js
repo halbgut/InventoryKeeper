b096d161dd362dfdb7f73be41261a4889a6c16dd
Kriegslustig <github@ls7.ch>
2015-03-05 21:39:14 +0100

    Add update form for Objects

--- client/router.js
+++ client/router.js
@@ -28,6 +28,28 @@ Router.route('/objects/new', {
   }
 })
 
+Router.route('/objects/:_id/update', {
+  name: 'objects.update'
+, waitOn: function () {
+    return Meteor.subscribe('allObjects')
+  }
+, onBeforeAction: function () {
+    var self = this
+    if(!Meteor.userId()) {
+      self.render('blockNotAllowed')
+    } else {
+      self.next()
+    }
+  }
+, action: function () {
+    var self = this
+    console.log(Objects.findOne({_id: this.params._id}))
+    self.render('blockObjectsUpdate', {data: {
+          doc: Objects.findOne({_id: this.params._id})
+        }})
+  }
+})
+
 Router.route('/login', {
   name: 'user.login'
 , action: function () {
