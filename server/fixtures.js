e5afcfe69d1797ede2a28181a7c28ea6f1aaef98
Kriegslustig <github@ls7.ch>
2015-03-05 20:53:12 +0100

    Add some fixtures

---
+++ server/fixtures.js
@@ -0,0 +1,17 @@
+if(Objects.find({}).count() < 1) {
+  Objects.insert({
+    name: 'JVC Camera'
+  , objectType: 'Camera'
+  , serialNo: 'XYS9448'
+  })
+  Objects.insert({
+    name: 'Panasonic Camera'
+  , objectType: 'Camera'
+  , serialNo: 'XYS9448'
+  })
+  Objects.insert({
+    name: 'Rode Mic'
+  , objectType: 'Mic'
+  , serialNo: '2391DSG'
+  })
+}
