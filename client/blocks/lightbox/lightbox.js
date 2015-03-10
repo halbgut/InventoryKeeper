549d940dd5b6999003bea2ce23da141041cf2f96
Kriegslustig <github@ls7.ch>
2015-03-10 21:40:01 +0100

    Add a basic lightbox block

---
+++ client/blocks/lightbox/lightbox.js
@@ -0,0 +1,43 @@
+Lightbox = {
+  config: {
+    openClass: 'lightbox--js--open'
+  }
+, init: function (route) {
+    var self = this
+    self.route = route
+    self.checkRoute()
+    self.addURLListeners()
+  }
+, addURLListeners: function () {
+    var self = this
+    addEventListener('hashchange', function () {
+      self.checkRoute()
+    })
+  }
+, checkRoute: function () {
+    var self = this
+  , hashSplit = location.href.split('#')
+    if(hashSplit[1] && hashSplit[1].indexOf(self.route) > -1) {
+      self.open()
+    } else {
+      self.close()
+    }
+  }
+, open: function () {
+    var self = this
+    if(self.element.className.indexOf(self.config.openClass) > -1) return
+    self.element.className += ' ' + self.config.openClass
+  }
+, close: function () {
+    var self = this
+    if(self.element.className.indexOf(self.config.openClass) < 0) return
+    self.element.className = self.element.className.replace(' ' + self.config.openClass, '')
+  }
+}
+
+Template.lightbox.rendered = function () {
+  var self = this
+  thisLightbox = Object.create(Lightbox)
+  thisLightbox.element = this.firstNode
+  thisLightbox.init(self.data.route)
+}
